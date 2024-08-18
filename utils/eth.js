65819a7a308b54196199facf88318abe729073e7
reid <reid@scogg.in>
2024-08-18 16:07:30 -0500

    revert eth.js

--- utils/eth.js
+++ utils/eth.js
@@ -69,7 +69,7 @@ async function getPrivateKey(argv){
     const kg = require('urbit-key-generation');
     let wallet = await kg.generateWallet({
       ticket: argv.privateKeyTicket,
-      ship: {decPoint},
+      ship: decPoint,
       boot: false,
       revision: 1
     });
