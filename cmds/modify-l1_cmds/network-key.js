dde13d995a4635f5f45b808e2c18fb0dca156817
reid <reid@scogg.in>
2024-09-11 10:38:20 -0500

    add breach flag for l1

--- cmds/modify-l1_cmds/network-key.js
+++ cmds/modify-l1_cmds/network-key.js
@@ -1,16 +1,22 @@
-const ob = require('urbit-ob')
-const ajs = require('azimuth-js')
-const _ = require('lodash')
-const {files, validate, eth, findPoints} = require('../../utils')
+const ob = require("urbit-ob");
+const ajs = require("azimuth-js");
+const _ = require("lodash");
+const { files, validate, eth, findPoints } = require("../../utils");
 
-exports.command = 'network-key'
-exports.desc = 'Set the network key for one or more points.'
+exports.command = "network-key";
+exports.desc = "Set the network key for one or more points.";
 
-exports.builder = function(yargs) {
-}
+exports.builder = function (yargs) {
+  yargs.option("breach", {
+    describe: "Do a factory reset of the ship.",
+    default: false,
+    type: "boolean",
+  });
+};
 
-exports.handler = async function (argv)
-{
+exports.builder = function (yargs) {};
+
+exports.handler = async function (argv) {
   const workDir = files.ensureWorkDir(argv.workDir);
   const privateKey = await eth.getPrivateKey(argv);
   const ctx = await eth.createContext(argv);
@@ -20,8 +26,7 @@ exports.handler = async function (argv)
   const points = findPoints.getPoints(argv, workDir, wallets);
 
   console.log(`Will set network keys for ${points.length} points`);
-  for (const p of points) 
-  {
+  for (const p of points) {
     let patp = ob.patp(p);
     console.log(`Trying to set network key for ${patp} (${p}).`);
 
@@ -29,56 +34,71 @@ exports.handler = async function (argv)
     let wallet = argv.useWalletFiles ? wallets[patp] : null;
     const revision = 1; //TODO: support bumping the revision (by looking it up on-chain)
     const keysFileName = `${patp.substring(1)}-networkkeys-${revision}.json`;
-    
+
     let networkKeyPair = null;
-    if(wallet){
+    if (wallet) {
       networkKeyPair = wallet.network.keys;
-    }
-    else if(files.fileExists(workDir, keysFileName)){
+    } else if (files.fileExists(workDir, keysFileName)) {
       networkKeyPair = files.readJsonObject(workDir, keysFileName);
-    }
-    else{
-      console.error(`Could not find network keys for ${patp}: provide them either via wallet or network key file.`);
+    } else {
+      console.error(
+        `Could not find network keys for ${patp}: provide them either via wallet or network key file.`,
+      );
       process.exit(1);
     }
 
-    var res = await ajs.check.canConfigureKeys(ctx.contracts, p, ethAccount.address);
-    if(!res.result){
-        console.log(`Cannot set network key for ${patp}: ${res.reason}`);
-        continue;
+    var res = await ajs.check.canConfigureKeys(
+      ctx.contracts,
+      p,
+      ethAccount.address,
+    );
+    if (!res.result) {
+      console.log(`Cannot set network key for ${patp}: ${res.reason}`);
+      continue;
     }
 
     //we are using the public keys because in the contract only the public keys should be visible, the private keys are used to generate the arvo key file
     var publicCrypt = ajs.utils.addHexPrefix(networkKeyPair.crypt.public);
     var publicAuth = ajs.utils.addHexPrefix(networkKeyPair.auth.public);
 
-    var existingKeys = await ajs.azimuth.getKeys(ctx.contracts, p);
-    if(existingKeys.crypt == publicCrypt && existingKeys.auth == publicAuth)
-    {
-        console.log(`The network key is already set for ${patp}`);
-        // console.log(JSON.stringify(networkKeyPair, null, 2));
-        // console.log(JSON.stringify(existingKeys, null, 2));
-        continue;
+    if (
+      currentKeys.crypt == publicCrypt &&
+      currentKeys.auth == publicAuth &&
+      !argv.breach
+    ) {
+      console.log(`The network key is already set for ${patp}`);
+      // console.log(JSON.stringify(networkKeyPair, null, 2));
+      // console.log(JSON.stringify(currentKeys, null, 2));
+      continue;
+    } else if (
+      currentKeys.crypt == publicCrypt &&
+      currentKeys.auth == publicAuth &&
+      argv.breach
+    ) {
+      console.log(`Breaching with existing key revision ${patp}.`);
     }
 
     // Create and Send Tx
     let tx = ajs.ecliptic.configureKeys(
-        ctx.contracts, 
-        p, 
-        publicCrypt, 
-        publicAuth,
-        CRYPTO_SUITE_VERSION,
-        false);
-    await eth.setGasSignSendAndSaveTransaction(ctx, tx, privateKey, argv, workDir, patp, 'networkkey');
+      ctx.contracts,
+      p,
+      publicCrypt,
+      publicAuth,
+      CRYPTO_SUITE_VERSION,
+      argv.breach,
+    );
+    await eth.setGasSignSendAndSaveTransaction(
+      ctx,
+      tx,
+      privateKey,
+      argv,
+      workDir,
+      patp,
+      "networkkey",
+    );
   } //end for each point
-  
+
   process.exit(0);
 };
 
 const CRYPTO_SUITE_VERSION = 1;
-
-
-
-
-
-
