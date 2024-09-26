e0b35db4e5301750772b41f56fc26b43d0bc99b2
reid <reid@scogg.in>
2024-09-26 00:41:47 -0700

    add yargs

--- cmds/get.js
+++ cmds/get.js
@@ -2,6 +2,16 @@ exports.command = "get <command>";
 exports.desc =
   "Retrieve information about Urbit points, L2 rollers, and Ethereum gas prices.";
 exports.builder = function (yargs) {
+  yargs.option("points", {
+    alias: ["p", "point"],
+    describe: `One or more points, can be p or patp.`,
+    type: "array",
+    conflicts: ["points-file", "use-wallet-files"],
+  });
+  yargs.option("adoptee", {
+    describe: `Ship to check for escape request.`,
+    type: "string",
+  });
   return yargs.commandDir("get_cmds");
 };
 exports.handler = function (argv) {};
